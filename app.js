0cd013581d6fbad3e96e474bfb01cce5ceb4397a
gabbar23 <amansaini842@gmail.com>
2022-09-05 17:13:24 +0530

    new

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("ho");
+// function add(n1:number,n2:number){
+//     console.log(n1+n2);
+function add(user) {
+    console.log(user.name);
+}
+console.log(add({ name: "name", age: 23 }));
--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log("ho~");
+// function add(n1:number,n2:number){
+//     console.log(n1+n2);
+
+//     return n1+n2;
+// }
+
+// // const number1=5;
+// const number2=4;
+
+// let number1=5
+// number1=5
+// add(number1,number2);
+
+type User = { name: string; age: number };
+ 
+ function add(user: User):void {
+  console.log(user.name);
+}
+console.log(
+add({ name: "name", age: 23 }));
