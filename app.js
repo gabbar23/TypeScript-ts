509f4a67730d12af97385a7c0c566e4c9f50fc07
gabbar23 <amansaini842@gmail.com>
2022-09-16 16:50:59 +0530

    n

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 "use strict";
-// function add(n1:number,n2:number){
-//     console.log(n1+n2);
-//     return n1+n2;
-// }
-// // const number1=5;
-// const number2=4;
-// let number1=5
-// number1=5
-// add(number1,number2);
-// type User = { name: string; age: number };
-// function add(user: User): void {
-//   console.log(user.name);
-// }
-// console.log(add({ name: "name", age: 23 }));
-// let combine= (a: number, b: string="1") => {console.log('gi');
-// };
-// let a:string[]
-// a=[1,2,3,'g']
-// console.log(a);
-const a = (...numbers) => console.log(numbers);
-a(1, 2, 1);
+// // function add(n1:number,n2:number){
+// //     console.log(n1+n2);
+// //     return n1+n2;
+// // }
+// // // const number1=5;
+// // const number2=4;
+// // let number1=5
+// // number1=5
+// // add(number1,number2);
+// // type User = { name: string; age: number };
+// // function add(user: User): void {
+// //   console.log(user.name);
+// // }
+// // console.log(add({ name: "name", age: 23 }));
+// // let combine= (a: number, b: string="1") => {console.log('gi');
+// // };
+// // let a:string[]
+// // a=[1,2,3,'g']
+// // console.log(a);
+// const a=(...numbers:number[])=>console.log(numbers);
+// a(1,2,1);
+class Main {
+    constructor(m) {
+        this.name = m;
+    }
+    describe() {
+        console.log(this.name);
+    }
+}
+const a1 = new Main("aman");
+// a1.describe();
+const onj = { describe: a1.describe };
+onj.describe.bind(a1);
--- app.ts
+++ app.ts
@@ -1,31 +1,52 @@
-// function add(n1:number,n2:number){
-//     console.log(n1+n2);
+// // function add(n1:number,n2:number){
+// //     console.log(n1+n2);
 
-//     return n1+n2;
-// }
+// //     return n1+n2;
+// // }
 
-// // const number1=5;
-// const number2=4;
+// // // const number1=5;
+// // const number2=4;
 
-// let number1=5
-// number1=5
-// add(number1,number2);
+// // let number1=5
+// // number1=5
+// // add(number1,number2);
 
-// type User = { name: string; age: number };
+// // type User = { name: string; age: number };
 
-// function add(user: User): void {
-//   console.log(user.name);
-// }
-// console.log(add({ name: "name", age: 23 }));
+// // function add(user: User): void {
+// //   console.log(user.name);
+// // }
+// // console.log(add({ name: "name", age: 23 }));
 
-// let combine= (a: number, b: string="1") => {console.log('gi');
-// };
+// // let combine= (a: number, b: string="1") => {console.log('gi');
+// // };
 
-// let a:string[]
-// a=[1,2,3,'g']
-// console.log(a);
+// // let a:string[]
+// // a=[1,2,3,'g']
+// // console.log(a);
 
-const a=(...numbers:number[])=>console.log(numbers);
+// const a=(...numbers:number[])=>console.log(numbers);
 
-a(1,2,1);
+// a(1,2,1);
 
+
+
+class Main{
+    name:string;
+
+    constructor(m:string){
+        this.name=m;
+    }
+
+    describe(this:Main){
+        console.log(this.name);
+        
+    }
+}
+
+const a1=new Main("aman");
+// a1.describe();
+
+
+const onj={describe:a1.describe}
+onj.describe.bind(a1);
